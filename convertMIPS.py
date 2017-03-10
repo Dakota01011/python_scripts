8c0bf9078e27cd4ea4f15fb8aac7af9484d38964
Nikki <dakotakoelling@gmail.com>
2017-03-10 00:07:06 +0000

    Bad copy for test

--- convertMIPS.py
+++ convertMIPS.py
@@ -11,22 +11,7 @@ reg1Pattern = re.compile(r"[ \t]*(\w+)[ \t]+\$(\w+)")
 reg1immPattern = re.compile(r"[ \t]*(\w+)[ \t]+\$(\w+),[ \t]+(-?\d+)")
 immPattern = re.compile(r"[ \t]*(\w+)[ \t]+(-?\d+)")
 syscallPattern = re.compile(r"[ \t]*(\w+)")
-regLookupTable = {'zero':0, 'at':1, 'v0':2, 'v1':3, 'a0':4, 'a1':5, 'a2':6, 'a3':7, 't0':8, 't1':9, 't2':10, 't3':11, 't4':12, 't5':13, 't6':14, 't7':15, 's0':16, 's1':17, 's2':18, 's3':19, 's4':20, 's5':21, 's6':22, 's7':23, 't8':24, 't9':25, 'k0':26, 'k1':27, 'gp':28, 'sp':29, 'fp':30, 'ra':31}
 
-def regNameToNumber(name):
-	if name in regLookupTable:
-		regNumber = regLookupTable[name]
-	else:
-		regNumber = int(name)
-	return regNumber
-
-def convertToNum(instruction):
-	instrHex = -1
-	if offsetPattern.match(instruction):
-		# OP $t, offset($s)
-		# ???? ??ss ssst tttt iiii iiii iiii iiii
-		matches = offsetPattern.match(instruction)
-		instr = matches.group(1)
 		reg1 = regNameToNumber(matches.group(2))
 		offset = int(matches.group(3))
 		reg2 = regNameToNumber(matches.group(4))
